a8bd274a1ca2d4fdeb08fcc4e3b624d478c17723
robintzeng <robintzeng@gmail.com>
2020-03-24 00:03:14 -0400

    final

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-<<<<<<< HEAD
-cmake -DCMAKE_PREFIX_PATH=/home/robin/libtorch ..
 
-when build in the build folder
+//cmake -DCMAKE_PREFIX_PATH=/home/robin/libtorch ..
+
+Change the CMAKE_PREFIX_PATH CMakeList to your libtorch path
+
+libtorch should download from
+https://download.pytorch.org/libtorch/cpu/libtorch-shared-with-deps-1.0.1.zip 
--- SuperPointPretrainC++.cpp
+++ SuperPointPretrainC++.cpp
@@ -2,25 +2,29 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <stdio.h>
+#include "SPextractor.h"
+
 using namespace cv;
+using namespace ORB_SLAM2;
 int main(int argc, char** argv )
-{
-    torch::Tensor tensor = torch::eye(3);
-    std::cout << tensor << std::endl;
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
-    Mat image;
-    image = imread( argv[1], 1 );
-    if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
-    }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
-    waitKey(0);
+{   
+    // Retrieve paths to images
+    Mat desc;
+    Mat im,im2;
+    std::vector<cv::KeyPoint> mvKeys;
+    cv::Mat mDescriptors;
+    im = imread("../data/123.png",CV_8UC1);
+    
+    ORBextractor* extractor = new ORBextractor(500,1.2,4,0.015,0.007);
+
+    (*extractor)(im,cv::Mat(),mvKeys,mDescriptors);
+
+    drawKeypoints(im, mvKeys, im2  ,Scalar::all(-1), DrawMatchesFlags::DEFAULT);
+    
+    //imshow("Display Image", im2);
+    //waitKey(0);   
+    imwrite( "Gray.jpg",im2);
+    
+    
     return 0;
 }
